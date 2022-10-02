b7b9306a9b361c9dcba6d17860242502037f755c
Aman <amanurumbekov@gmail.com>
2022-10-02 23:10:24 +0600

    Create sqrt_method.cpp

---
+++ prime-numbers/sqrt_method.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool is_prime(unsigned long long num)
+{
+    if (num < 2){
+        return false;
+    }
+
+    if (num == 2 || num == 3){
+        return true;
+    }
+    
+    if (num % 2 == 0 || num % 3 == 0){
+        return false;
+    }
+
+    for (unsigned long long i = 5; i <= sqrt(num); i+=6){
+        if (num % i == 0 || num % (i + 2) == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    unsigned long long num;
+    cin >> num;
+
+    is_prime(num) ? cout << "true\n" : cout << "false\n";
+    return 0;
+}
